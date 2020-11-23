809573cb7b0ce8ac288ddc8eb8b6577d5ea9db45
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-23 15:23:34 +0000

    testing what we have done so far

--- star_wars.sql
+++ star_wars.sql
@@ -1,4 +1,6 @@
 DROP TABLE characters;
+DROP TABLE lightsabers;
+
 CREATE TABLE characters (
     name VARCHAR(255),
     darkside BOOLEAN,
@@ -7,14 +9,19 @@ CREATE TABLE characters (
 );
 
 
-CREATE TABLE lightsabers (
+CREATE table lightsabers (
     id SERIAL,
-    hilt_metal VARCHAR(255),
+    hilt_metal varchar(255),
     colour VARCHAR(255)
 );
+
+
+insert INTO lightsabers (colour, hilt_metal) values ('green', 'palladium');
+INSERT INTO lightsabers (colour, hilt_metal) VALUES ('red', 'gold');
 
 
 INSERT INTO characters (name, darkside, age) VALUES ('Obi-Wan Kenobi', false, 27);
 INSERT INTO characters (name, darkside, age) VALUES ('Anakin Skywalker', false, 19);
 INSERT INTO characters (name, darkside, age) VALUES ('Darth Maul', true, 32);
 SELECT * FROM characters ;
+SELECT * FROM lightsabers ;
