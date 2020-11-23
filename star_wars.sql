42102989a4313bac2c24a3d4ef31cc993d746686
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-23 15:20:05 +0000

    new sql file added

--- star_wars.sql
+++ star_wars.sql
@@ -1,8 +1,4 @@
--- Just to make sure that the table you are creating doesn't already exist.
 DROP TABLE characters;
-
-
--- Now create the table
 CREATE TABLE characters (
     name VARCHAR(255),
     darkside BOOLEAN,
@@ -11,32 +7,14 @@ CREATE TABLE characters (
 );
 
 
--- Insert data into the table
-INSERT INTO characters (name, darkside, age) VALUES('Obi-Wan Kenobi', false, 27);
-INSERT INTO characters (name, darkside, age) VALUES('Anakin Skywalker', false, 19);
-INSERT INTO characters (name, darkside, age) VALUES('Darth Maul', true, 32);
+CREATE TABLE lightsabers (
+    id SERIAL,
+    hilt_metal VARCHAR(255),
+    colour VARCHAR(255)
+);
 
-SELECT * FROM characters;
 
-SELECT name, age FROM characters WHERE darkside = true;
-SELECT COUNT(*) FROM characters;
-SELECT COUNT(*) FROM characters WHERE darkside = true;
-SELECT COUNT(*) FROM characters WHERE name = 'Anakin Skywalker';
-
-UPDATE characters SET darkside = true WHERE name = 'Anakin Skywalker';
-SELECT * FROM characters;
-
-UPDATE characters SET (darkside, age) = (false, 25) WHERE name = 'Anakin Skywalker';
-SELECT * FROM characters;
-
-DELETE FROM characters WHERE name = 'Darth Maul';
-
-SELECT * FROM characters;
-
-SELECT * FROM characters WHERE id = 3; -- this id does NOT exist
-SELECT * FROM characters WHERE id = 2; -- this id does exist
-
-INSERT INTO characters (name, darkside, age) VALUES('Darth Vader', true, 45);
-
-SELECT * FROM characters;
-
+INSERT INTO characters (name, darkside, age) VALUES ('Obi-Wan Kenobi', false, 27);
+INSERT INTO characters (name, darkside, age) VALUES ('Anakin Skywalker', false, 19);
+INSERT INTO characters (name, darkside, age) VALUES ('Darth Maul', true, 32);
+SELECT * FROM characters ;
